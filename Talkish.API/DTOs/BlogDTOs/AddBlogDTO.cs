9368819d82d0d1528e9d3ad34fac3ce3ba2f72fb
Vlad Mihet <vlad.mihet@amdaris.com>
2022-06-05 13:58:33 +0300

    refactor: remove model required title and content validation

--- Talkish.API/DTOs/BlogDTOs/AddBlogDTO.cs
+++ Talkish.API/DTOs/BlogDTOs/AddBlogDTO.cs
@@ -1,16 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Talkish.API.DTOs
 {
     public class AddBlogDTO
     {
-        [Required]
         [Display(Name = "Blog Title")]
         [DataType(DataType.Text)]
         public string Title { get; set; }
 
-        [Required]
         [DataType(DataType.Text)]
         [Display(Name = "Blog Content")]
         public string Content { get; set; }
--- Talkish.Domain/Models/Blog.cs
+++ Talkish.Domain/Models/Blog.cs
@@ -9,12 +9,10 @@ namespace Talkish.Domain.Models
         [Key]
         public int BlogId { get; set; }
 
-        [Required]
         [Display(Name = "Blog Title")]
         [DataType(DataType.Text)]
         public string Title { get; set; }
 
-        [Required]
         [DataType(DataType.Text)]
         [Display(Name = "Blog Content")]
         public string Content { get; set; }
