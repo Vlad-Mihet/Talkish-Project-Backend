a1b6db9874282536f555b5e11cdab48fce2522f8
Vlad Mihet <vlad.mihet@amdaris.com>
2022-06-05 13:15:09 +0300

    refactor: remove title & content validation for blog creation to facilitate front-end logic for draft blogs creation (Which might be almost empty)

--- Talkish.API/DTOs/BlogDTOs/AddBlogDTO.cs
+++ Talkish.API/DTOs/BlogDTOs/AddBlogDTO.cs
@@ -8,13 +8,11 @@ namespace Talkish.API.DTOs
         [Required]
         [Display(Name = "Blog Title")]
         [DataType(DataType.Text)]
-        [StringLength(100, MinimumLength = 3, ErrorMessage = "Blog Title must be longer than 3 characters, but shorter than 100")]
         public string Title { get; set; }
 
         [Required]
         [DataType(DataType.Text)]
         [Display(Name = "Blog Content")]
-        [StringLength(100000, MinimumLength = 30, ErrorMessage = "Blog content must be longer than 30 characters")]
         public string Content { get; set; }
 
         public bool IsDraft { get; set; }
--- Talkish.Domain/Models/Blog.cs
+++ Talkish.Domain/Models/Blog.cs
@@ -12,13 +12,11 @@ namespace Talkish.Domain.Models
         [Required]
         [Display(Name = "Blog Title")]
         [DataType(DataType.Text)]
-        [StringLength(100, MinimumLength = 3, ErrorMessage = "Blog Title must be longer than 3 characters, but shorter than 100")]
         public string Title { get; set; }
 
         [Required]
         [DataType(DataType.Text)]
         [Display(Name = "Blog Content")]
-        [StringLength(100000, MinimumLength = 30, ErrorMessage = "Blog content must be longer than 30 characters")]
         public string Content { get; set; }
 
         [Display(Name = "Draft Story")]
